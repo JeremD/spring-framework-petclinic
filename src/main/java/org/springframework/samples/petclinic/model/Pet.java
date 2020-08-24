53306f0217a24131d3c86bd0fe248963ce00dce2
JeremYD <delpech.jeremy@hotmail.fr>
2020-08-24 11:55:43 +0200

    Test erreur compilation

--- src/main/java/org/springframework/samples/petclinic/model/Pet.java
+++ src/main/java/org/springframework/samples/petclinic/model/Pet.java
@@ -48,11 +48,11 @@ public class Pet extends NamedEntity {
 
     @Column(name = "birth_date")
     @DateTimeFormat(pattern = "yyyy/MM/dd")
-    //private LocalDate birthDate;
+    private LocalDate birthDate;
 
     @ManyToOne
     @JoinColumn(name = "type_id")
-    private PetType type;
+    //private PetType type;
 
     @ManyToOne
     @JoinColumn(name = "owner_id")
